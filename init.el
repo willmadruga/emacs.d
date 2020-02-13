d1dd2a6219629257900751a6428e6628b0bc1b72
William Madruga <willmadruga@gmail.com>
2020-02-13 10:56:34 -0800

    changed font-size

--- init.el
+++ init.el
@@ -25,5 +25,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-(put 'downcase-region 'disabled nil)
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 120 :width normal)))))
+
+;; init.el ends here
+
